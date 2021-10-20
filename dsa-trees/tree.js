78dca590da3df5e5cdbc648ba26b4197b556017e
khagerman <71734063+khagerman@users.noreply.github.com>
2021-10-20 14:00:35 -0500

    completed trees

--- dsa-trees/tree.js
+++ dsa-trees/tree.js
@@ -23,6 +23,19 @@ class Tree {
       }
     }
   }
+  // look at everything at one level before going down
+  findBFS(val) {
+    const toVisitQueue = [this];
+    while (toVisitQueue.length) {
+      const current = toVisitQueue.shift();
+      if (current.val === val) {
+        return current;
+      }
+      for (let child of current.children) {
+        toVisitQueue.push(child);
+      }
+    }
+  }
 
   /** sumValues(): add up all of the values in the tree. */
 
@@ -43,12 +56,41 @@ class Tree {
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
-  countEvens() {}
+  countEvens() {
+    if (!this.root) return 0;
+    let total = this.root.val % 2 === 0 ? 1 : 0;
+    function even(node) {
+      for (let child of node.children) {
+        if (child.val % 2 === 0) {
+          total++;
+        }
+        if (child.children.length > 0) {
+          even(child);
+        }
+      }
+    }
+    even(this.root);
+    return total;
+  }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
-  numGreater(lowerBound) {}
+  numGreater(lowerBound) {
+    if (!this.root) return 0;
+    let total = this.root.val > lowerBound ? 1 : 0;
+    function findGreater(node) {
+      for (let child of node.children) {
+        if (child.val > lowerBound) {
+          total++;
+        }
+        if (child.children.length > 0) {
+          findGreater(child);
+        }
+      }
+    }
+    findGreater(this.root);
+    return total;
+  }
 }
-
 module.exports = { Tree, TreeNode };
