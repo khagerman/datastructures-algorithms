82b562f86db0e4828a094c5b6debfdcc5938f2dd
khagerman <71734063+khagerman@users.noreply.github.com>
2021-10-22 16:26:05 -0500

    saving finished problems

--- dsa-trees/binary-tree.js
+++ dsa-trees/binary-tree.js
@@ -8,6 +8,13 @@ class BinaryTreeNode {
   }
 }
 
+// Why not variable called total and to total
+// Adding to [this]
+// This meaning in binary search trees
+// visualize recursion
+// how do you use data structures day to day
+// when to use helper dunction and when not to in recursion
+// there was something elese
 class BinaryTree {
   constructor(root = null) {
     this.root = root;
@@ -17,59 +24,134 @@ class BinaryTree {
    * the length of the shortest path from the root to a leaf. */
 
   minDepth() {
+    // if no root return 0
+    if (!this.root) return 0;
+    function _minDepth(node) {
+      // if get to end of the line (leaf) return 1
+      if (node.left === null && node.right === null) return 1;
+      // if left is empty check right and add 1
+      if (node.left === null) return _minDepth(node.right) + 1;
+      // if left is empty check right and add one
+      if (node.right === null) return _minDepth(node.left) + 1;
+      // check each side and add one
+      return Math.min(_minDepth(node.left), _minDepth(node.right)) + 1;
+    }
 
+    return _minDepth(this.root);
   }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
    * the length of the longest path from the root to a leaf. */
 
   maxDepth() {
+    if (!this.root) return 0;
+    let result = 0;
+    function _maxDepth(node) {
+      // if it is a leaf return 1
+      if (node.left === null && node.right === null) return 1;
 
+      if (node.left === null) return _maxDepth(node.right) + 1;
+      if (node.right === null) return _maxDepth(node.left) + 1;
+      return Math.max(_maxDepth(node.left), _maxDepth(node.right)) + 1;
+    }
+
+    return _maxDepth(this.root);
   }
 
   /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
    * The path doesn't need to start at the root, but you can't visit a node more than once. */
+  //
 
   maxSum() {
+    let result = 0;
+    function sum(node) {
+      if (node === null) return 0;
+      // find sum of left branch
+      const leftSum = sum(node.left);
+      // find sum of right branch
+      const rightSum = sum(node.right);
+      // see if result is larget so far
+      result = Math.max(result, node.val + leftSum + rightSum);
 
+      return Math.max(0, leftSum + node.val, rightSum + node.val);
+    }
+    sum(this.root);
+    return result;
   }
-
   /** nextLarger(lowerBound): return the smallest value in the tree
    * which is larger than lowerBound. Return null if no such value exists. */
-
+  // findBFS(val) {
+  //   const toVisitQueue = [this];
+  //   while (toVisitQueue.length) {
+  //     const current = toVisitQueue.shift();
+  //     if (current.val === val) {
+  //       return current;
+  //     }
+  //     for (let child of current.children) {
+  //       toVisitQueue.push(child);
+  //     }
+  //   }
+  // }
   nextLarger(lowerBound) {
-
-  }
-
-  /** Further study!
-   * areCousins(node1, node2): determine whether two nodes are cousins
-   * (i.e. are at the same level but have different parents. ) */
-
-  areCousins(node1, node2) {
-
-  }
-
-  /** Further study!
-   * serialize(tree): serialize the BinaryTree object tree into a string. */
-
-  static serialize() {
-
-  }
-
-  /** Further study!
-   * deserialize(stringTree): deserialize stringTree into a BinaryTree object. */
-
-  static deserialize() {
-
-  }
-
-  /** Further study!
-   * lowestCommonAncestor(node1, node2): find the lowest common ancestor
-   * of two nodes in a binary tree. */
-
-  lowestCommonAncestor(node1, node2) {
-    
+    if (this.root === null) return null;
+    let queue = [this.root];
+    let final = null;
+    while (queue.length) {
+      let currentNode = queue.shift();
+      if (
+        currentNode.val > lowerBound &&
+        (currentNode.val < final || final === null)
+      ) {
+        final = currentNode.val;
+      }
+      if (currentNode.right) queue.push(currentNode.right);
+      if (currentNode.left) queue.push(currentNode.left);
+    }
+    return final;
   }
 }
+
+//   // let's use BFS for this!
+//   let queue = [this.root];
+//   let closest = null;
+
+//   while (queue.length) {
+//     let currentNode = queue.shift();
+//     let currentVal = currentNode.val;
+//     let higherThanLowerBound = currentVal > lowerBound;
+//     let shouldReassignClosest = currentVal < closest || closest === null;
+
+//     if (higherThanLowerBound && shouldReassignClosest) {
+//       closest = currentVal;
+//     }
+
+//     if (currentNode.left) queue.push(currentNode.left);
+//     if (currentNode.right) queue.push(currentNode.right);
+//   }
+
+//   return closest;
+// }
+
+// /** Further study!
+//  * areCousins(node1, node2): determine whether two nodes are cousins
+//  * (i.e. are at the same level but have different parents. ) */
+
+// areCousins(node1, node2) {}
+
+// /** Further study!
+//  * serialize(tree): serialize the BinaryTree object tree into a string. */
+
+// static serialize() {}
+
+// /** Further study!
+//  * deserialize(stringTree): deserialize stringTree into a BinaryTree object. */
+
+// static deserialize() {}
+
+// /** Further study!
+//  * lowestCommonAncestor(node1, node2): find the lowest common ancestor
+//  * of two nodes in a binary tree. */
+
+// lowestCommonAncestor(node1, node2) {}
 
 module.exports = { BinaryTree, BinaryTreeNode };
