52e3375b979684669735af1176642cecf632b4cf
khagerman <71734063+khagerman@users.noreply.github.com>
2021-10-27 15:39:25 -0500

    finishing bubble sort

--- dsa-sorting/bubble.js
+++ dsa-sorting/bubble.js
@@ -2,9 +2,13 @@ function bubbleSort(arr) {
   let isSorted = false;
   while (!isSorted) {
     isSorted = true;
+    // Iterate until we get to the last element
     for (let i = 1; i < arr.length; i++) {
+      // If the element to the left is bigger, then swap the element
+      // that we're currently looking at with its left neighbor.
       if (arr[i - 1] > arr[i]) {
         isSorted = false;
+        // Swap elements by creating a temporary reference.
         let temp = arr[i - 1];
         arr[i - 1] = arr[i];
         arr[i] = temp;
